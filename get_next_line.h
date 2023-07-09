f83e1d3b51f7b48789ef319579fa169cdd3dafe3
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-07-09 12:27:07 +0200

    upload gnl h with correct norm

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvidot <cvidot@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 16:57:40 by cvidot            #+#    #+#             */
+/*   Updated: 2023/07/09 12:24:29 by cvidot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# define BUFFER_SIZE 42
+
+int		ft_strlen(char *s);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
